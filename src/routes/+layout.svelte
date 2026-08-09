95171137787f978c3f3a6c3dddd965a76253a086
Mahdeen Khan <mahdeenkhan99@gmail.com>
2026-08-09 15:16:22 -0400

    adapter static for build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+	
+	// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
+	// pages by setting this here, but should prerender as many as possible to
+	// avoid large performance and SEO impacts
+	export const prerender = true;
 </script>
 
 <svelte:head>
